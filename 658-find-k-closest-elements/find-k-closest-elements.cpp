465946763018c235b8e23dd8254d6a38b4d24c14
Tanay Singh Chauhan <tanays.chauhan.cer24@itbhu.ac.in>
2026-07-31 16:20:34 +0530

    Time: 4 ms (50.33%) | Memory: 42.2 MB (6.62%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<pair<int,int>> v;
+        for(int i = 0 ; i < arr.size() ; i++){
+            v.push_back({abs(x-arr[i]) , arr[i]});
+        }
+        sort(v.begin(),v.end());
+        vector<int> ans;
+        for(int i = 0 ; i < k ; i ++){
+            ans.push_back(v[i].second);
+        }
+        sort(ans.begin() , ans.end());
+        return ans;
+    }
+};
