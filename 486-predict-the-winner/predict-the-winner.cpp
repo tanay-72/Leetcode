82566fb23d882e95c2999cbc8af93f00e4900f80
Tanay Singh Chauhan <tanays.chauhan.cer24@itbhu.ac.in>
2026-08-01 15:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (96.48%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    bool solve(vector<int> &nums , int l , int r , int s1 , int s2 , int turn){
-        if(l > r){
-            return s1 >= s2;
-        }
-
-        if(turn == 0){
-            bool f1 = solve(nums , l +1 , r , s1 + nums[l] , s2 , 1);
-            bool f2 = solve(nums , l , r-1 , s1 + nums[r] , s2, 1);
-            return f1 || f2 ;
-        }
-        
-            bool f1 = solve(nums , l + 1 , r , s1 , s2 + nums[l] , 0);
-            bool f2 = solve(nums , l , r - 1 , s1 , s2 + nums[r] , 0 );
-        return f1 && f2 ;
+    int dp[21][21];
+    int solve(vector<int> &nums , int i , int j){
+        if(i > j) return 0 ;
+        if(i == j) return nums[i];
+        if(dp[i][j] != -1) return dp[i][j];
+        int takei = nums[i] + min(solve(nums , i+1 , j-1) , solve(nums , i+2 , j));
+        int takej = nums[j] + min(solve(nums , i , j-2) , solve(nums , i+1 , j-1));
+        return dp[i][j] = max(takei , takej);
     }
     bool predictTheWinner(vector<int>& nums) {
-        return solve(nums , 0 , nums.size()-1 , 0 , 0 , 0);
+        memset(dp , -1 , sizeof(dp));
+        int sum = accumulate(nums.begin() , nums.end() , 0);
+        int p1 =  solve(nums , 0 , nums.size()-1);
+        return p1 >= sum - p1;
     }
 };
