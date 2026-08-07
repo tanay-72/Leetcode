ed0793bb32910fb528cb88e518e4903e0cb3816f
Tanay Singh Chauhan <tanays.chauhan.cer24@itbhu.ac.in>
2026-08-08 01:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (52.68%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp1(n,0);
+        dp1[0] = nums[0];
+        dp1[1] = max(nums[0] , nums[1]);
+        for(int i = 2 ; i < n-1 ; i++){
+            dp1[i] = max(dp1[i-1] , nums[i] + dp1[i-2]);
+        }
+        vector<int> dp2(n,0);
+        dp2[1] = nums[1];
+        for(int i = 2 ; i < n ; i ++){
+            dp2[i] = max(dp2[i-2]+nums[i] , dp2[i-1]); 
+        }
+        return max(dp1[n-2] , dp2[n-1]);
+    }   
+};
+
+// rob from 1st till 2nd last
+// rob from 2nd to last
+// rob from 2 to 2ndlast
